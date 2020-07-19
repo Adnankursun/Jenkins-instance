8e0043c2adccee3bdfeb887bc8cde5f83b14d4fb
adnan kursun <54333199+Adnankursun@users.noreply.github.com>
2020-07-19 02:32:46 -0500

    Update main.tf

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -2,7 +2,7 @@ provider "aws" {}
 
 resource "aws_instance" "jenkins" {
   ami           = "ami-02ed69c3babb71502"
-  ami           = "ami-014e6ee78ce685d56"
+  
   instance_type = "t2.micro"
 
   tags = {
