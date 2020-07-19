6db2b0b3d16036bccb43b1688bb5cf6d3fad76a6
adnankursun <adnankursun@live.com>
2020-07-19 03:11:37 -0500

    removed some files

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,10 +1,7 @@
 provider "aws" {}
 
 resource "aws_instance" "jenkins" {
-  ami           = "ami-02ed69c3babb71502" # eu-west-1"
-  ami           = "ami-014e6ee78ce685d56" # us-east-1"
-  ami           = "ami-0841302c04aa480c1" # us-west-1"
-  
+    
   instance_type = "t2.micro"
 
   tags = {
