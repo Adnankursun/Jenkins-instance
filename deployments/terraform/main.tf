6590a0e10934f05f88c9598ae1d917bc17787a04
adnankursun <adnankursun@live.com>
2020-07-24 23:06:10 -0500

    ami

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -2,7 +2,7 @@ provider "aws" {}
 
 resource "aws_instance" "jenkins" {
  
- ami           = "ami-02ed69c3babb71502" # eu-west-1"
+ ami           = "ami-0c3e74fa87d2a4227" # eu-west-1"
  # ami           = "ami-014e6ee78ce685d56" # us-east-1"
  # ami           = "ami-0841302c04aa480c1" # us-west-1"
 
