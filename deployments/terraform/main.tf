edd047a51f0ebcafd0ea602d28dd765256ea95f6
adnankursun <adnankursun@live.com>
2020-07-25 16:37:46 -0500

    added  some lines

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,12 +1,11 @@
 provider "aws" {}
 
 resource "aws_instance" "jenkins" {
-  ami           = "${var.ami}"
+  ami           = "${var.ami_id}"
   instance_type = "t2.micro"
-  # key_name      = "${aws_key_pair.jenkins_key.key_name}"
   tags = {
-    Name = "${var.Name}"
-  }
+    name = "${var.Name}"
+    }
 }
-variable "ami" {}
-variable "Name" {}
+variable "ami_id" {}
+variable "name" {}
