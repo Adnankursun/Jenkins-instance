bf2ca70de6c3d29ca96bf46f2050b1a2b4c324cc
adnankursun <adnankursun@live.com>
2020-07-29 10:58:07 -0500

    added name tag

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -3,7 +3,11 @@ provider "aws" {}
 resource "aws_instance" "jenkins" {
   ami           = "${var.ami_id}"
   instance_type = "t2.micro"
+  tags = {
+    name = "${var.instance_name}"
+    }
+
   
 }
 variable "ami_id" {}
-
+variable "instance_name" {}
